7cf2b064ce43a9f01e819b71db3498295f247a6a
Pei <stevenkaoo@gmail.com>
2020-02-16 20:49:59 +1100

    Updates

---
+++ precache-manifest.12ae30b355d5e97e2e0fbc4850ac1a15.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3683734c2cb6814d300ad2dfdd2d33d7",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "293b9d751bc028c06394",
+    "url": "/personal-website/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5a6574179063f7b73cca",
+    "url": "/personal-website/static/css/main.ea4fb2c5.chunk.css"
+  },
+  {
+    "revision": "293b9d751bc028c06394",
+    "url": "/personal-website/static/js/2.911492f9.chunk.js"
+  },
+  {
+    "revision": "5a6574179063f7b73cca",
+    "url": "/personal-website/static/js/main.edb5013d.chunk.js"
+  },
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  },
+  {
+    "revision": "377138a8e99bd4421d9eaea013613aa6",
+    "url": "/personal-website/static/media/Bootstrap_Pattern.377138a8.jpg"
+  },
+  {
+    "revision": "c73ea182b6ea1fdab7b4e950e67b4649",
+    "url": "/personal-website/static/media/Bootstrap_Pattern.c73ea182.png"
+  },
+  {
+    "revision": "f6bfa4af72ea9f8973b00ba2a0b3a9f6",
+    "url": "/personal-website/static/media/Timesheet.f6bfa4af.jpg"
+  },
+  {
+    "revision": "440b82bafd0da9bc814d04147f1528b4",
+    "url": "/personal-website/static/media/Timesheet2.440b82ba.jpg"
+  },
+  {
+    "revision": "e84ffb1e0837fa4b31885d703e84ed42",
+    "url": "/personal-website/static/media/myImage.e84ffb1e.jpg"
+  },
+  {
+    "revision": "a6767f38122614de375755667c0fc451",
+    "url": "/personal-website/static/media/poker.a6767f38.jpg"
+  },
+  {
+    "revision": "1d74db3b3ca653718c5da5e328d7eee4",
+    "url": "/personal-website/static/media/react_todo.1d74db3b.jpg"
+  },
+  {
+    "revision": "ac82d97d170d742395675707f640e98d",
+    "url": "/personal-website/static/media/todolist.ac82d97d.png"
+  }
+]);
